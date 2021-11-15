3112a0b3e041e1fb3c328ae6c22517fe1e104dc6
SERKAN UĞUR <79767737+serkanugur525@users.noreply.github.com>
2021-11-15 21:23:13 +0300

    Middleware, Migration

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth:', 'verified'])->get('/panel', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group(['middleware'=>['auth','isAdmin']],function() {
-    Route::get('deneme',function() {
-
-        return "middleware testi";
-
-    });
-
-
+Route::group(['middleware'=>['auth','isAdmin'],'prefix'=>'admin'],function(){
 
 });
